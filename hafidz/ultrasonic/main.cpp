d7c0c7767768358ea0281dc7510ec00c43c63112
virmansza <hafidzfirman2006@gmail.com>
2025-09-20 20:54:34 +0700

    push hafidz

---
+++ hafidz/ultrasonic/main.cpp
@@ -0,0 +1,29 @@
+// C++ code
+//
+
+const int trigPin = 11;
+const int echoPin = 12;
+
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+  Serial.print("arak: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
+}
